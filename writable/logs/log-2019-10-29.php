b52740f9cd521a2c2725fcd786c1892bb55a180d
Ahern <1358945354@qq.com>
2019-10-30 09:03:30 +0800

    Add Lab5

---
+++ writable/logs/log-2019-10-29.php
@@ -0,0 +1,23 @@
+<?php defined('SYSTEMPATH') || exit('No direct script access allowed'); ?>
+
+CRITICAL - 2019-10-29 19:53:26 --> Class 'App\Models\Places' not found
+#0 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\COMP4711\\XML...')
+#5 {main}
+CRITICAL - 2019-10-29 19:53:32 --> Class 'App\Models\Places' not found
+#0 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Places->index()
+#1 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Places))
+#2 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\COMP4711\\XML...')
+#5 {main}
+CRITICAL - 2019-10-29 19:53:37 --> Class 'App\Models\Places' not found
+#0 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(844): App\Controllers\Travel->index()
+#1 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(335): CodeIgniter\CodeIgniter->runController(Object(App\Controllers\Travel))
+#2 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\CodeIgniter.php(245): CodeIgniter\CodeIgniter->handleRequest(NULL, Object(Config\Cache), false)
+#3 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\public\index.php(45): CodeIgniter\CodeIgniter->run()
+#4 D:\COMP4711\XMLPP1\htdocs\dgpt4711lab05\vendor\codeigniter4\framework\system\Commands\Server\rewrite.php(34): require_once('D:\\COMP4711\\XML...')
+#5 {main}
